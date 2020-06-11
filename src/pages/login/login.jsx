c107d579be5b630ee6d1fa36a7e161f06765fa7e
CodingKL <lpshfish@gmail.com>
2020-06-11 14:21:44 +0900

    Form的声明式验证

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -36,6 +36,10 @@ class Login extends Component {
         console.log('handleSubmit()', values)
     }
 
+    // this.props.form.getFieldDecorator(id, options)
+    //          https://3x.ant.design/components/form-cn/
+    //  
+
     // 组件的数据包括外部传入的和自身的数据!!!
     render () {
 
@@ -53,8 +57,25 @@ class Login extends Component {
                     <h2>用户登陆</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
+                            {
+                                    /*
+                                        用户名/密码的合法性要求
+                                            1.必须输入
+                                            2.必须大于4位 
+                                            3.必须小于12位
+                                            4.必须是英文, 数字或下划线组成 
+                                    */
+                            }
                             {getFieldDecorator('username', { 
-                                rules: [{ required: true, message: 'Please input your username!' }],
+                                /* 指定验证规则,  配置对象: 属性名是特定的一些名称, */
+                                /*   /^[a-zA-Z0-9_]+$/ 有了+号可以匹配任意多的字符 */
+                                // 声明式验证, 直接使用别人定义好的验证规则进行验证!!!
+                                rules: [
+                                    { required: true, whitespace: true, message: '用户名必须输入' },
+                                    { min: 4, message: '用户名至少4位' },
+                                    { max: 12, message: '用户名最多12位' },
+                                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文, 数字或下划线组成' }
+                                ],
                             })(
                                 <Input
                                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -98,6 +119,8 @@ class Login extends Component {
     //      2.必须大于4位 
     //      3.必须小于12位
     //      4.必须是英文, 数字或下划线组成
+    //
+    // 验证时机有两个:    输入框输入数据的时候, 点击登陆按钮提交的时候
     //
     /*
     render() {
@@ -146,7 +169,33 @@ class Login extends Component {
 
 // 返回一个新的组件
 // 高阶函数   create, 因为它返回的是一个函数
+//      1).一类特别的函数
+//              a.接收函数类型的参数
+//              b.函数的返回值是函数
+//      2).常见的高阶函数
+//              a.定时器:    setTimeout() / setInterval()
+//              b.Promise:  Promise(() => {})  then(value => {}, reason => {})
+//              c.数组遍历相关的方法:  forEach()/filter()/map()/reduce()/find()/findIndex()              
+//              d.函数对象的bind方法, 返回一个新的方法:   fn.bind()
+//              e.Form.create()()  /  getFieldDecorator()()
+//
+//      3).高阶函数更新动态, 更加具有扩展性
+//            一个函数代表了一种动态的功能, 函数如果能接收一个函数, 就接受了一种变化的功能, 返回一个函数就返回了以后一种可以反复使用的功能!!!
+//
+
 // 高阶组件
+//          1).本质就是一个函数
+//          2).参数接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性, 这两个组件的关系是父子组件
+//          3).作用:   扩展组件的功能
+//          4).高阶组件也是高阶函数:  接收一个组件函数, 返回是一个新的组件函数
+//
+//  渲染包装以后的高阶组件WrapLogin:    const WrapLogin = Form.create()(Login), 有了包装组件, login组件才能进一步完成所有其它的功能
+//  
+//  组件与组件的标签对象的关系;   组件是一个类型, 标签是组件的某一个实例, 高阶组件是用来包装组件类型的, 不是实例!!!
+//
+//  高阶组件(组件函数)   ------>   返回一个新的组件
+//  
+//
 
 /*
         包装Form组件, 生成一个新的组件: Form(login)
